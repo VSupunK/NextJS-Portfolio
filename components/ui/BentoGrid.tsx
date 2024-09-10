a18a63ee668cb2912be9422ded4bf92d085ad4f6
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-10 11:50:48 +0530

    Grid updated

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,3 +1,5 @@
+import { BackgroundGradientAnimation } from "./GradientBg";
+
 import { cn } from "@/lib/utils";
 
 export const BentoGrid = ({
@@ -20,13 +22,13 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
-  id, 
-  title, 
-  description, 
-  className, 
-  img, 
-  imgClassName, 
-  titleClassName, 
+  id,
+  title,
+  description,
+  className,
+  img,
+  imgClassName,
+  titleClassName,
   spareImg
 }: {
   className?: string;
@@ -35,25 +37,50 @@ export const BentoGridItem = ({
   header?: React.ReactNode;
   icon?: React.ReactNode;
   id?: number;
-  img ?: string; 
-  imgClassName ?: string; 
-  titleClassName ?: string; 
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
   spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent flex flex-col space-y-4",
         className
       )}
       style={{
-        background: "rgb(4,7,29)",
-        backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+        background: "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
+      <div className={cn(id === 6 ? "flex justify-center h-full" : "")}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img
+              src={img}
+              alt="img"
+              className={cn(imgClassName, "object-cover object-center")}
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <div className="absolute right-0 -bottom-5 flex justify-center h-full">
+            <div className="w-full h-full absolute">
+              {spareImg && (
+                <img
+                  src={spareImg}
+                  alt="spareImg"
+                  className="object-cover object-center w-full h-full"
+                />
+              )}
+            </div>
 
+            {id === 6 && (
+              <BackgroundGradientAnimation>
+                <div className="absolute z-50 flex items-center justify-center text-white" />
+              </BackgroundGradientAnimation>
+            )}
+          </div>
+        )}
       </div>
 
       <div className="group-hover/bento:translate-x-2 transition duration-200">
