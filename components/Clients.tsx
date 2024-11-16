9fd15a8c027aba6b767b75d221e6f8afd67bf1a9
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-11-16 14:56:43 +0530

    Update Hero component introduction, modify layout metadata, and enhance Clients component with hover effects

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -4,10 +4,10 @@ import { companies, testimonials } from "@/data";
 
 function Clients() {
   return (
-    <div className="py-20 " id="testimonials">
+    <div className="py-20" id="testimonials">
       <h1 className="heading">
         Inspiring Insights from{" "}
-        <span className="text-purple"> Visionaries in Technology</span>
+        <span className="text-purple">Visionaries in Technology</span>
       </h1>
       <div className="flex flex-wrap items-center max-lg:mt-10">
         <InfiniteMovingCards
@@ -17,14 +17,26 @@ function Clients() {
         />
       </div>
       <h1 className="heading pb-5">
-        My <span className="text-purple"> Tech Stack</span>
+        My <span className="text-purple">Tech Stack</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center g4 md:gap-10 max-lg:mt-8">
+      <div className="flex flex-wrap items-center justify-center g4 md:gap-6 max-lg:mt-8">
         {companies.map(({ id, img, name }) => (
-          <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
-            <img src={img} alt={name} className="md:w-22 w-20" />
-
-            {/* <img src={nameImg} alt={name} className="md:w-24 w-20" /> */}
+          <div
+            key={id}
+            className="group flex flex-col items-center justify-center gap-2"
+          >
+            <div className="relative">
+              {/* Icon with hover effect */}
+              <img
+                src={img}
+                alt={name}
+                className="md:w-22 w-20 transition-transform duration-300 group-hover:scale-125"
+              />
+            </div>
+            {/* Name displayed on hover */}
+            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center text-lg text-gray-100">
+              {name}
+            </span>
           </div>
         ))}
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Supun, a Web Developer.
+            Hi, I&apos;m Supun Kalhara, a Fullstack Developer | UI/UX Designer.
           </p>
 
           <a href="#about">
